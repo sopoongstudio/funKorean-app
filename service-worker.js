cf6209c432c018a984e67a8616a6f1e24de274c4
sopoongstudio <melanie700r@gmail.com>
2026-06-25 20:25:18 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'funkorean-v1';
+const ASSETS = [
+  '/hangul_learn_v2.html',
+  '/manifest.json',
+  '/icons/icon_192.png',
+  '/icons/icon_512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
